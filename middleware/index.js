4f4b37cc31709aaaba4efebe003ce65612ae4630
TEQ <dpooja@teqmavens.com>
2023-04-11 11:31:19 +0530

    middleware

---
+++ middleware/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express();
+
+require('dotenv').config()
+
+// const postsRouter = require('./routes/posts.router')
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+// Middleware authentication start**********
+const auth = require('./middleware/posts.middleware')
+app.use('/login',
+    auth,
+    (req, res) => {
+        res.status(200).send("Welcome! you are logged in successfully")
+    })
+// Middleware authentication end**********
+
+// app.use('/', postsRouter)
+
+const PORT = process.env.PORT || 9000
+
+app.listen(PORT, () => {
+    console.log("Server running...")
+})
